aeef1dc6eb7b05877946c0c68944b97657936d98
seoxubuntu <icyveins@hotmail.com>
2022-09-11 23:45:16 +0800

    add widget elements to demodWindow, hook up options UI slots
    resampling factors should be calculated correctly (need to test more);
    also need to fix that baud rate is currently limited to 99

--- demodWindow.py
+++ demodWindow.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QFormLayout, QWidget, QLabel, QComboBox, QPushButton
+from PySide6.QtWidgets import QSpinBox
 from PySide6.QtCore import Qt, Signal, Slot, QRectF
 import pyqtgraph as pg
 import numpy as np
@@ -19,13 +20,40 @@ class DemodWindow(QMainWindow):
 
         # Main layout
         widget = QWidget()
-        self.layout = QHBoxLayout()
+        self.layout = QVBoxLayout()
         widget.setLayout(self.layout)
         self.setCentralWidget(widget)
 
+        self.topLayout = QHBoxLayout()
+        self.layout.addLayout(self.topLayout)
+        self.btmLayout = QHBoxLayout()
+        self.layout.addLayout(self.btmLayout)
+
+        # Plots
+        self.setupPlots() # This has to be before setupOptions for layout reasons
+
         # Options menus
+        self.setupOptions()
+
+        # Object holder for the demodulator
+        self.demodulator = None
+
+    def setupPlots(self):
+        self.abswin = pg.GraphicsLayoutWidget()
+        self.topLayout.addWidget(self.abswin)
+        self.absplt = self.abswin.addPlot()
+        self.abspltitem = self.absplt.plot(np.arange(self.slicedData.size)/self.fs, np.abs(self.slicedData))
+
+        self.rwin = pg.GraphicsLayoutWidget()
+        self.btmLayout.addWidget(self.rwin)
+        self.eoplt = self.rwin.addPlot(0,0)
+        self.conplt = self.rwin.addPlot(0,1)
+
+
+
+    def setupOptions(self):
         self.optOuterLayout = QVBoxLayout()
-        self.layout.addLayout(self.optOuterLayout)
+        self.topLayout.addLayout(self.optOuterLayout)
 
         self.optLayout = QFormLayout()
         self.optOuterLayout.addLayout(self.optLayout)
@@ -36,12 +64,55 @@ class DemodWindow(QMainWindow):
         self.modDropdown.currentIndexChanged.connect(self.makeDemodulator)
         self.optLayout.addRow("Modulation Type", self.modDropdown)
 
+        self.fsLabel = QLabel(str(self.fs))
+        self.optLayout.addRow("Input Sample Rate: ", self.fsLabel)
+
+        self.baud = 1
+        self.baudSpinbox = QSpinBox()
+        self.baudSpinbox.setMinimum(1)
+        self.baudSpinbox.valueChanged.connect(self.setBaud)
+        self.optLayout.addRow("Baud Rate", self.baudSpinbox)
+
+        self.osrSpinbox = QSpinBox()
+        self.osr = 1
+        self.osrSpinbox.setMinimum(1)
+        self.osrSpinbox.valueChanged.connect(self.osrChanged)
+        self.optLayout.addRow("Target OSR", self.osrSpinbox)
+
+        self.updownLabel = QLabel()
+        self.optLayout.addRow("Resample Factors: ", self.updownLabel)
+
+        self.finalfsLabel = QLabel()
+        self.optLayout.addRow("Output Sample Rate: ", self.finalfsLabel)
+
+        # Call the slot once to initialize the other values
+        self.osrChanged(self.osr) 
+
         self.demodBtn = QPushButton("Demodulate")
         self.demodBtn.clicked.connect(self.runDemod)
         self.optOuterLayout.addWidget(self.demodBtn)
 
-        # Object holder for the demodulator
-        self.demodulator = None
+    @Slot(int)
+    def setBaud(self, baud):
+        self.baud = baud
+        # Re-evaluate the resampling factors
+        self.evaluateResampling()
+
+
+    @Slot(int)
+    def osrChanged(self, osr):
+        self.osr = osr
+        # Re-evaluate the resampling factors
+        self.evaluateResampling()
+
+    def evaluateResampling(self):
+        # Evaluate the resample factors
+        self.up = np.lcm(self.fs, self.osr * self.baud) // self.fs
+        self.down = np.lcm(self.fs, self.osr * self.baud) // (self.baud * self.osr)
+        self.finalfs = self.osr * self.baud
+        # Place them in their widgets
+        self.updownLabel.setText("%d/%d" % (self.up, self.down))
+        self.finalfsLabel.setText("%f" % self.finalfs)
 
     @Slot(int)
     def makeDemodulator(self, modidx: int):
@@ -61,6 +132,7 @@ class DemodWindow(QMainWindow):
 
     @Slot()
     def runDemod(self):
+
         print("TODO: runDemod")
 
 
