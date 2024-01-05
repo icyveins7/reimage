9dc1b49047503cf639357337db6ba6dc391bbf74
icyveins7 <icyveins@hotmail.com>
2024-01-05 14:29:05 +0800

    add median filter red line plot to fftWindow

--- fftWindow.py
+++ fftWindow.py
@@ -30,17 +30,26 @@ class FFTWindow(QMainWindow):
         if startIdx is not None and endIdx is not None:
             self.p.setLabels(title="Sample %d to %d" % (startIdx, endIdx))
         self.plt = pg.PlotDataItem()
+        self.pltmed = pg.PlotDataItem()
         self.p.addItem(self.plt)
+        self.p.addItem(self.pltmed)
         
         # Create the options
         self.fftlenLayout = QHBoxLayout()
         self.fftlenLabel = QLabel("FFT Length: ")
         self.fftlenLayout.addWidget(self.fftlenLabel)
         self.setupFFTDropdown()
+
+        self.medfiltLabel = QLabel("Median filter length: ")
+        self.fftlenLayout.addWidget(self.medfiltLabel)
+        self.setupFFTMedfiltDropdown()
         self.layout.addLayout(self.fftlenLayout)
 
         # Plot the data
+        self.fftData = None
+        self.medfiltData = None
         self.plot()
+        self.plot_medfilt()
 
     def setupFFTDropdown(self):
         self.fftlenDropdown = QComboBox()
@@ -48,13 +57,36 @@ class FFTWindow(QMainWindow):
         self.fftlenLayout.addWidget(self.fftlenDropdown)
         self.fftlenDropdown.activated.connect(self.on_fftlen_selected)
 
+    def setupFFTMedfiltDropdown(self):
+        self.fftmedfiltDropdown = QComboBox()
+        self.fftmedfiltDropdown.addItems([str(i) for i in np.arange(10)*2 + 15]) # Needs to be odd numbers
+        self.fftlenLayout.addWidget(self.fftmedfiltDropdown)
+        self.fftmedfiltDropdown.activated.connect(self.on_fftmedfilt_selected)
+
     @Slot()
     def on_fftlen_selected(self):
         # Replot
         self.plot()
 
     def plot(self):
-        f = np.fft.fft(self.slicedData, int(self.fftlenDropdown.currentText()))
+        self.fftData = np.fft.fft(self.slicedData, int(self.fftlenDropdown.currentText()))
+        self.fftData = np.fft.fftshift(self.fftData)
         self.plt.setData(
             x=np.fft.fftshift(makeFreq(int(self.fftlenDropdown.currentText()), self.fs)),
-            y=20*np.log10(np.abs(np.fft.fftshift(f))))
+            y=20*np.log10(np.abs(self.fftData)))
+        
+    @Slot()
+    def on_fftmedfilt_selected(self):
+        # Replot
+        self.plot_medfilt()
+
+    def plot_medfilt(self):
+        self.medfiltData = sps.medfilt(
+            np.abs(self.fftData),
+            kernel_size=int(self.fftmedfiltDropdown.currentText())
+        )
+        self.pltmed.setData(
+            x=np.fft.fftshift(makeFreq(int(self.fftlenDropdown.currentText()), self.fs)),
+            y=20*np.log10(self.medfiltData),
+            pen='r'
+        )
