65106c79cd551c9a7afd3c470bd887378416567c
seomac <icyveins@hotmail.com>
2024-05-30 11:05:38 +0800

    add screenshots folder to build

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller --noconsole --add-data .\matlab:.\matlab --add-data .\README.md:. --add-data .\icon.png:. --add-data .\ipc.py:. main.py
+pyinstaller --noconsole --add-data .\matlab:.\matlab --add-data .\README.md:. --add-data .\icon.png:. --add-data .\ipc.py:. --add-data .\screenshots:.\screenshots main.py
